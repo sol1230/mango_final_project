06443367b8806844139fa7dabd11c2f1de7e9025
sohiekim65 <sohiekim65@gmail.com>
2023-03-05 15:02:30 +0900

    package folder name modify

--- src/main/java/com/mango/mango_final_project/Controller/ByPassController.java
+++ src/main/java/com/mango/mango_final_project/Controller/ByPassController.java
@@ -1,4 +1,4 @@
-package com.mango.mango_final_project.controller;
+package com.mango.mango_final_project.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
